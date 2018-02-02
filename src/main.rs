51eb7265203317fdc76f9e0eb078c998153bcb09
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2018-02-03 02:19:53 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,8 @@ extern crate futures_cpupool;
 #[macro_use]
 extern crate serde;
 
-use finchers::{Application, Endpoint, EndpointServiceExt};
+use finchers::{Endpoint, EndpointServiceExt};
+use finchers::application::{Application};
 use finchers_json::JsonResponder;
 use Response::*;
 
@@ -100,58 +101,55 @@ pub fn build_endpoint() -> impl Endpoint<Item = Response> + 'static {
     use finchers::endpoint::prelude::*;
     use finchers_json::json_body;
     use finchers_urlencoded::{form_body, from_csv, queries_req};
+    use model::*;
 
     #[derive(Debug, Deserialize)]
     struct FindByStatusParam {
         #[serde(deserialize_with = "from_csv")]
-        status: Vec<model::Status>,
+        status: Vec<Status>,
     }
 
     #[derive(Debug, Deserialize)]
     struct UpdatePetViaFormParam {
         name: Option<String>,
-        status: Option<model::Status>,
+        status: Option<Status>,
     }
 
-    let pet_api = choice![
-        post("pet").with(json_body::<model::Pet>()).map(|_| Dummy),
-        put("pet").with(json_body::<model::Pet>()).map(|_| Dummy),
-        get("pet/findByStatus")
+    let pet_id = path::<PetId>();
+    let pet_body = json_body::<Pet>();
+    let pet_api = endpoint("pet").with(choice![
+        post(pet_body).map(|_| Dummy),
+        put(pet_body).map(|_| Dummy),
+        get("findByStatus")
             .with(queries_req::<FindByStatusParam>())
             .map(|_| Dummy),
-        get("pet").with(path::<model::PetId>()).map(|_| Dummy),
-        post("pet")
-            .with(path::<model::PetId>())
-            .join(form_body::<UpdatePetViaFormParam>())
-            .map(|_| Dummy),
-        delete("pet").with(path::<model::PetId>()).map(|_| Dummy),
-    ];
+        get(pet_id).map(|_| Dummy),
+        post((pet_id, form_body::<UpdatePetViaFormParam>())).map(|_| Dummy),
+        delete(pet_id).map(|_| Dummy),
+    ]);
 
-    let store_api = choice![
-        get("store/inventory").map(|_| Dummy),
-        post("store/order")
-            .with(json_body::<model::Order>())
-            .map(|_| Dummy),
-        get("store/order")
-            .with(path::<model::OrderId>())
-            .map(|_| Dummy),
-        delete("store/order")
-            .with(path::<model::OrderId>())
-            .map(|_| Dummy),
-    ];
+    let order_id = path::<OrderId>();
+    let order_body = json_body::<Order>();
+    let store_api = endpoint("store").with(choice![
+        get("inventory").map(|_| Dummy),
+        endpoint("order").with(choice![
+            post(order_body).map(|_| Dummy),
+            get(order_id).map(|_| Dummy),
+            delete(order_id).map(|_| Dummy),
+        ]),
+    ]);
 
-    let user_api = choice![
-        post("user").with(json_body::<model::User>()).map(|_| Dummy),
-        post("user/createWithArray")
-            .with(json_body::<Vec<model::User>>())
+    let username = path::<String>();
+    let user_body = json_body::<User>();
+    let user_api = endpoint("user").with(choice![
+        post(user_body).map(|_| Dummy),
+        post("createWithArray")
+            .with(json_body::<Vec<User>>())
             .map(|_| Dummy),
-        get("user").with(path::<String>()).map(|_| Dummy),
-        put("user")
-            .with(path::<String>())
-            .join(json_body::<model::User>())
-            .map(|_| Dummy),
-        delete("user").with(path::<String>()).map(|_| Dummy),
-    ];
+        get(username).map(|_| Dummy),
+        put((username, user_body)).map(|_| Dummy),
+        delete(username).map(|_| Dummy),
+    ]);
 
     endpoint("v2").with(choice![pet_api, store_api, user_api,])
 }
